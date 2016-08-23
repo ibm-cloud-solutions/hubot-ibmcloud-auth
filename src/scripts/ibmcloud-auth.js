1f08da350d58a8e56063afde3b6c99c43ca14676
Chris Hambridge <chambrid@us.ibm.com>
2016-08-23 16:30:14 -0400

    Add ldap capabilities to authorization checking to allow group authorization management.

--- src/scripts/ibmcloud-auth.js
+++ src/scripts/ibmcloud-auth.js
@@ -14,6 +14,7 @@
 */
 'use strict';
 
+var ldap = require('ldapjs');
 var path = require('path');
 var TAG = path.basename(__filename);
 const env = require(path.resolve(__dirname, '..', 'lib', 'env'));
@@ -101,7 +102,12 @@ var POWER_COMMANDS = [
 
 var POWER_EMAILS = [];
 var READER_EMAILS = [];
+var POWER_GROUPS = [];
+var READER_GROUPS = [];
 var HUBOT_IBMCLOUD_AUTHENTICATION_DISABLED = env.authenticationDisabled ? env.authenticationDisabled : false;
+
+var ldapClient = null;
+var ldapConnected = false;
 
 if (env.powerUsers) {
 	POWER_EMAILS = env.powerUsers.split(',');
@@ -110,6 +116,56 @@ if (env.powerUsers) {
 if (env.readerUsers) {
 	READER_EMAILS = env.readerUsers.split(',');
 }
+
+function ldapInit() {
+	var promise = new Promise((resolve, reject) => {
+		if (env.ldapServer && env.ldapPort && env.ldapBindUser && env.ldapBindPassword) {
+			if (env.ldapPowerGroups === ''){
+				POWER_GROUPS = [];
+			} else {
+				POWER_GROUPS = env.ldapPowerGroups.split(';');
+			}
+			if (env.ldapReaderGroups === ''){
+				READER_GROUPS = [];
+			} else {
+				READER_GROUPS = env.ldapReaderGroups.split(';');
+			}
+
+			if (bot) {
+				bot.logger.debug(`${TAG}: LDAP POWER_GROUPS=${POWER_GROUPS}`);
+				bot.logger.debug(`${TAG}: LDAP READER_GROUPS=${READER_GROUPS}`);
+			}
+
+			var ldapUrl = `${env.ldapProtocol}://${env.ldapServer}:${env.ldapPort}`;
+			if (bot) {
+				bot.logger.info(`${TAG}: Attemping to connect to LDAP at ${ldapUrl}.`);
+			}
+
+			ldapClient = ldap.createClient({
+				url: ldapUrl,
+				connectTimeout: 5000,
+				timeout: 10000
+			});
+			ldapClient.bind(env.ldapBindUser, env.ldapBindPassword, function(err) {
+				if (err) {
+					if (bot) {
+						bot.logger.error(`${TAG}: An error was hit during LDAP binding. Cannot connect to ldap at ${ldapUrl}`);
+						bot.logger.error(err);
+					}
+				} else {
+					ldapConnected = true;
+					if (bot) {
+						bot.logger.info(`${TAG}: Connected to LDAP at ${ldapUrl} sucessfully.`);
+					}
+				}
+				resolve(ldapConnected);
+			});
+		} else {
+			resolve(true);
+		}
+	});
+	return promise;
+};
 
 function checkAuthorization(context, next, done) {
 	if (HUBOT_IBMCLOUD_AUTHENTICATION_DISABLED) {
@@ -136,45 +192,219 @@ function checkAuthorization(context, next, done) {
 	}
 
 	var unauthorized = false;
+	var authorizedReader = false;
+	var authorizedPower = false;
+	isAuthorizedReader(emailAddress).then(authReader => {
+		authorizedReader = authReader;
+		return isAuthorizedPowerUser(emailAddress);
+	}).then(authPower => {
+		authorizedPower = authPower;
+		if (isReaderCommand(commandId) && (!authorizedReader && !authorizedPower)) {
+			unauthorized = true;
+		} else if (isPowerCommand(commandId) && !authorizedPower) {
+			unauthorized = true;
+		}
 
-	if (isReaderCommand(commandId) && (!isAuthorizedReader(emailAddress) && !isAuthorizedPowerUser(
-		emailAddress))) {
-		unauthorized = true;
-	} else if (isPowerCommand(commandId) && !isAuthorizedPowerUser(emailAddress)) {
-		unauthorized = true;
-	}
-
-	if (unauthorized) {
+		if (unauthorized) {
+			if (bot) {
+				bot.logger.info(`${TAG}: User ${emailAddress} is not authorized to use command ${commandId}.`);
+			}
+			let msg = i18n.__('no.access');
+			context.response.reply(msg);
+			done();
+		} else {
+			next();
+		}
+	})
+	.catch(err => {
 		if (bot) {
-			bot.logger.info(`${TAG}: User ${emailAddress} is not authorized to use command ${commandId}.`);
+			bot.logger.error(`${TAG}: An error occurred during authorization checks:`);
+			bot.logger.error(err);
 		}
 		let msg = i18n.__('no.access');
 		context.response.reply(msg);
 		done();
-	} else {
-		next();
-	}
-}
-
-module.exports = function(robot) {
-	bot = robot;
-	robot.listenerMiddleware(checkAuthorization);
-	robot.on('ibmcloud-auth-to-nlc', (res, authEmitParams) => {
-		checkAuthorization({response: res, commandId: authEmitParams.emitTarget}, function next(){
-			// Forward the request to the given emit target with the given parameters
-			bot.logger.debug(`Auth granted for NLC command. Emitting to ${authEmitParams.emitTarget} with params ${authEmitParams.emitParameters}`);
-			bot.emit(authEmitParams.emitTarget, res, authEmitParams.emitParameters);
-		}, function done() {
-		});
 	});
 };
 
+module.exports = function(robot) {
+	bot = robot;
+	ldapInit().then(connected => {
+		robot.listenerMiddleware(checkAuthorization);
+		robot.on('ibmcloud-auth-to-nlc', (res, authEmitParams) => {
+			checkAuthorization({response: res, commandId: authEmitParams.emitTarget}, function next(){
+				// Forward the request to the given emit target with the given parameters
+				bot.logger.debug(`Auth granted for NLC command. Emitting to ${authEmitParams.emitTarget} with params ${authEmitParams.emitParameters}`);
+				bot.emit(authEmitParams.emitTarget, res, authEmitParams.emitParameters);
+			}, function done() {
+			});
+		});
+	})
+	.catch(err => {
+		bot.logger.error(`${TAG}: An error occurred during authorization initialization:`);
+		bot.logger.error(err);
+	});
+};
+
+function ldapSearch(query, filter) {
+	if (bot) {
+		bot.logger.debug(`${TAG}: ldapSearch(${query}, ${filter})`);
+	}
+	var promise = new Promise((resolve, reject) => {
+		var entryFound = false;
+		var opts = {
+			filter: filter,
+			scope: 'sub'
+		};
+		ldapClient.search(query, opts, function(err, res) {
+			if (err) {
+				if (bot) {
+					bot.logger.error(`${TAG}: An error was hit during LDAP search.`);
+					bot.logger.error(err);
+				}
+				throw err;
+			}
+			res.on('searchEntry', function(entry) {
+				if (bot) {
+					bot.logger.debug(`${TAG}: ldap filter ${filter} was matched for query ${query}`);
+				}
+				entryFound = true;
+				resolve(entry);
+			});
+			res.on('error', function(err) {
+				if (bot) {
+					bot.logger.error(`${TAG}: An error was hit during LDAP search.`);
+					bot.logger.error(err);
+				}
+				reject(err);
+			});
+			res.on('end', function(result) {
+				if (!entryFound) {
+					if (bot) {
+						bot.logger.debug(`${TAG}: ldap filter ${filter} was not matched for query ${query}`);
+					}
+					resolve(null);
+				}
+			});
+		});
+	});
+	return promise;
+
+}
+
+function getDistinguishedName(email) {
+	var filter = `(${env.ldapEmailField}=${email})`;
+	return ldapSearch(env.ldapOrgRoot, filter).then(entry => {
+		if (entry === null) {
+			return Promise.resolve(null);
+		} else {
+			return Promise.resolve(entry.objectName);
+		}
+	});
+};
+
+function isMemberOfGroup(email, group) {
+	return getDistinguishedName(email).then(dn => {
+		if (dn === null) {
+			return Promise.resolve(false);
+		} else {
+			var filter = `(${env.ldapGroupMembershipField}=${dn})`;
+			return ldapSearch(group, filter);
+		}
+	})
+	.catch(err => {
+		if (bot) {
+			bot.logger.error(`${TAG}: An error was hit during getting the distinguished name.`);
+			bot.logger.error(err);
+		}
+		return Promise.reject(err);
+	});
+};
+
+function isMemberOfAGroup(email, groupArray) {
+	if (bot) {
+		bot.logger.debug(`${TAG}: Test if ${email} is a member of groupArray:` + JSON.stringify(groupArray));
+	}
+	var promise = new Promise((resolve, reject) => {
+		if (!Array.isArray(groupArray)) {
+			resolve(false);
+		} else {
+			if (groupArray.length <= 0) {
+				resolve(false);
+			} else {
+				var promiseArray = [];
+				groupArray.forEach((group, index) => {
+					promiseArray.push(isMemberOfGroup(email, group));
+				});
+				Promise.all(promiseArray).then(values => {
+					var result = false;
+					values.forEach((val, idx) => {
+						if (val !== null && val !== false) {
+							result = true;
+						}
+					});
+					if (bot) {
+						bot.logger.debug(`${TAG}: ${email} group membership test is ${result} for groupArray:` + JSON.stringify(groupArray));
+					}
+					resolve(result);
+				})
+				.catch(err => {
+					if (bot) {
+						bot.logger.error(`${TAG}: An error occured while testing group membership.`);
+						bot.logger.error(err);
+					}
+					resolve(false);
+				});
+			}
+		}
+	});
+	return promise;
+};
+
 function isAuthorizedReader(email) {
-	return (READER_EMAILS.indexOf(email) !== -1);
+	var promise = new Promise((resolve, reject) => {
+		if (READER_EMAILS.indexOf(email) !== -1) {
+			resolve(true);
+		} else {
+			if (ldapConnected) {
+				isMemberOfAGroup(email, READER_GROUPS).then(result => {
+					resolve(result);
+				}).catch(err => {
+					if (bot) {
+						bot.logger.error(`${TAG}: An error was hit during LDAP group membership lookup.`);
+						bot.logger.error(err);
+					}
+					resolve(false);
+				});
+			} else {
+				resolve(false);
+			}
+		}
+	});
+	return promise;
 }
 
 function isAuthorizedPowerUser(email) {
-	return (POWER_EMAILS.indexOf(email) !== -1);
+	var promise = new Promise((resolve, reject) => {
+		if (POWER_EMAILS.indexOf(email) !== -1) {
+			resolve(true);
+		} else {
+			if (ldapConnected) {
+				isMemberOfAGroup(email, POWER_GROUPS).then(result => {
+					resolve(result);
+				}).catch(err => {
+					if (bot) {
+						bot.logger.error(`${TAG}: An error was hit during LDAP group membership lookup.`);
+						bot.logger.error(err);
+					}
+					resolve(false);
+				});
+			} else {
+				resolve(false);
+			}
+		}
+	});
+	return promise;
 }
 
 function isReaderCommand(commandId) {
